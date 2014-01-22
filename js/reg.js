f9b7666bb0226d392afc9e3019ad8c94a3936adc
yu <1905784504@qq.com>
2014-01-22 10:56:56 +0800

    登录页面已完成

--- js/reg.js
+++ js/reg.js
@@ -1,12 +1,37 @@
 // JavaScript Document
 $(function() {
+	$('#username').focus(function() {
+		$('#J_usernameTips').hide();
+	});
+	$('#pwd').focus(function() {
+		$('#J_pwdTips').hide();
+	});	
+	
+	//点击登录按钮后验证表单通过则提交数据
 	$('#J_regBtn').click(function() {
 		var username = $('#username').val();
 		var pwd = $('#pwd').val();
-		$.post('', {}, function(data) {
-			if(data) {
-			}
-			if(data) {
+		//var usertype = $('radio[name=usertype]:checked').val();
+		if(!username) {
+			$('#J_usernameTips').show();
+			return;
+		}
+		if(!pwd) {
+			$('#J_pwdTips').show();
+			return;
+		}
+		
+		$.post('user!login', {'username':username,'pwd':pwd}, function(data) {	
+			//-2为用户名不存在
+			//-1为用户名和密码不匹配
+			//1则为通过，跳转到个人中心页面	
+			if(data==-2) {
+				$('#J_usernameTips').text('您输入用账户名不存在').show();
+			}else if(data==-1) {
+				$('#J_pwdTips').text('您输入的账户名和密码不匹配').show();
+			}else if(data==1) {
+				alert('登陆成功');
+				//location.href = '';
 			}
 		});
 	});
